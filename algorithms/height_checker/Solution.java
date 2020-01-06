88b93bd75a5f1da96df40abd7aa1acd1f4d3f4a2
ravi-1996 <ravi@vassarlabs.com>
2020-01-06 11:35:57 +0530

    Leetcode :: Algorithms :: Height Checker Solution

---
+++ algorithms/height_checker/Solution.java
@@ -0,0 +1,53 @@
+/*
+Height Checker
+
+Problem Statement:
+
+Students are asked to stand in non-decreasing order of heights for an annual photo.
+
+Return the minimum number of students not standing in the right positions.  (This is the number of students that must move in order for all students to be standing in non-decreasing order of height.)
+
+ 
+Example 1:
+
+Input: [1,1,4,2,1,3]
+Output: 3
+Explanation: 
+Students with heights 4, 3 and the last 1 are not standing in the right positions.
+
+Note:
+
+    1 <= heights.length <= 100
+    1 <= heights[i] <= 100
+
+*/
+
+class Solution {
+    public int heightChecker(int[] heights) {
+        int count = 0;
+        int tempArr[] = new int[heights.length];
+        
+        for(int i=0;i<tempArr.length;i++) {
+            tempArr[i] = heights[i];
+        }
+        
+        Arrays.sort(tempArr);
+        
+        for(int i=0;i<tempArr.length;i++) {
+            if(tempArr[i] != heights[i]) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
+
+
+/*
+
+	Time complexity -- O(nlogn)
+
+	Space complexity -- O(n)
+
+*/
