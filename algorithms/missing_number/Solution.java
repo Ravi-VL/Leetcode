08da9e14e68b01b566405caad64ad933c9eead37
ravi-1996 <ravi@vassarlabs.com>
2020-01-08 11:38:52 +0530

    Leetcode :: Algorithms :: Missing Number Solution

---
+++ algorithms/missing_number/Solution.java
@@ -0,0 +1,44 @@
+/*
+Missing Number
+
+Problem Statement:
+
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1:
+
+	Input: [3,0,1]
+	Output: 2
+
+Example 2:
+
+	Input: [9,6,4,2,3,5,7,0,1]
+	Output: 8
+
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+
+*/
+
+// Difficulty Level -- Easy
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sumTillN = ((nums.length+1) * nums.length)/2;
+        int sumOfGivenArr = 0;
+        for(int i=0;i<nums.length;i++) {
+            sumOfGivenArr += nums[i];
+        }
+        
+        return (sumTillN-sumOfGivenArr);
+    }
+}
+
+/*
+
+	Time complexity -- O(n)
+
+	Space complexity -- O(1)
+
+*/
+
