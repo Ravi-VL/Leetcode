9b7501381fa3c520fde0163d0baa6ba1aba1bce9
ravi-1996 <ravi@vassarlabs.com>
2020-01-10 17:43:15 +0530

    Leetcode :: Algorithms :: Count and Say Solution

---
+++ algorithms/count_and_say/Solution.java
@@ -0,0 +1,102 @@
+/*
+Count and Say
+
+Problem Statement:
+
+The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+
+Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence. You can do so recursively, in other words from the previous member read off the digits, counting the number of digits in groups of the same digit.
+
+Note: Each term of the sequence of integers will be represented as a string.
+
+Example 1:
+
+	Input: 1
+	Output: "1"
+	Explanation: This is the base case.
+
+Example 2:
+
+	Input: 4
+	Output: "1211"
+	Explanation: For n = 3 the term was "21" in which we have two groups "2" and "1", "2" can be read as "12" which means frequency = 1 		and value = 2, the same way "1" is read as "11", so the answer is the concatenation of "12" and "11" which is "1211".
+
+*/
+
+//Difficulty Level -- Easy
+
+class Solution {
+    public String countAndSay(int n) {
+        
+        String output[] = new String[n];
+        String first = "1";
+        String second = "11";
+        
+        if(n==1) {
+            return first;
+        }else {
+            if(n>1) {
+                if(n==2) {
+                    return second;
+                }
+                String number = second;
+                for(int i=2; i<n; i++) {
+                    number = generateNumber(number);
+                }
+                return number;
+            }
+        }
+        return "";
+    }
+    
+    public static String generateNumber(String num) {
+        int length = num.length();
+        char arr[] = num.toCharArray();
+
+        int count=0,j=0;
+        StringBuilder out = new StringBuilder();
+
+        for(int i=0;i<arr.length;i++) {
+
+            if(arr[j] != arr[i]) {
+                if(count>0) {
+                    out.append(count);
+                }
+                out.append(arr[j]);
+                count=0;
+                j=i;
+            }
+
+            if(arr[j] == arr[i]) {
+                count++;
+                if(i==arr.length-1) {
+                    out.append(count);
+                    out.append(arr[j]);
+                }
+            }
+
+        }
+        return out.toString();
+    }
+    
+}
+
+
+/*
+
+	Time complexity -- O(n)
+
+	Space complexity -- O(n)
+
+*/
+
