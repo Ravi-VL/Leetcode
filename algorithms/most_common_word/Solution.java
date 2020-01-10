16d0f1d12b3d86975b02d587baec55a59dd6d030
ravi-1996 <ravi@vassarlabs.com>
2020-01-10 15:32:16 +0530

    Leetcode :: Algorithms :: Most Common Word Solution

---
+++ algorithms/most_common_word/Solution.java
@@ -0,0 +1,88 @@
+/*
+Most Common Word
+
+Problem Statement:
+
+Given a paragraph and a list of banned words, return the most frequent word that is not in the list of banned words.  It is guaranteed there is at least one word that isn't banned, and that the answer is unique.
+
+Words in the list of banned words are given in lowercase, and free of punctuation.  Words in the paragraph are not case sensitive.  The answer is in lowercase.
+
+Example:
+
+	Input: 
+	paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+	banned = ["hit"]
+
+	Output: "ball"
+
+	Explanation: 
+	"hit" occurs 3 times, but it is a banned word.
+	"ball" occurs twice (and no other word does), so it is the most frequent non-banned word in the paragraph. 
+	Note that words in the paragraph are not case sensitive,
+	that punctuation is ignored (even if adjacent to words, such as "ball,"), 
+	and that "hit" isn't the answer even though it occurs more because it is banned.
+
+Note:
+
+    1 <= paragraph.length <= 1000.
+    0 <= banned.length <= 100.
+    1 <= banned[i].length <= 10.
+    The answer is unique, and written in lowercase (even if its occurrences in paragraph may have uppercase symbols, and even if it is a proper noun.)
+    paragraph only consists of letters, spaces, or the punctuation symbols !?',;.
+    There are no hyphens or hyphenated words.
+    Words only consist of letters, never apostrophes or other punctuation symbols.
+
+*/
+
+//Difficulty Level -- Easy
+
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        PriorityQueue<Map.Entry<String, Integer>> maxHeap = 
+            new PriorityQueue<Map.Entry<String, Integer>>( (a,b)-> b.getValue()-a.getValue() );
+        
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        
+        String strs[] = paragraph.split("[!;:', ?.@]+");
+        
+        for(String str: strs) {
+            map.put(str.toLowerCase(), map.getOrDefault(str.toLowerCase(), 1)+1);
+        }
+        
+        for(Map.Entry<String, Integer> entry: map.entrySet()) {
+            maxHeap.add(entry);
+        }
+        
+        while(maxHeap.size() > 0) {
+            if(inBannedStrings(maxHeap.peek().getKey(), banned)) {
+                maxHeap.remove();
+            }else {
+                return maxHeap.peek().getKey();
+            }
+        }
+        
+        return "";
+    }
+    
+    public boolean inBannedStrings(String key, String banned[]) {
+        for(String str: banned) {
+            if(key.equalsIgnoreCase(str)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+}
+
+/*
+
+	Time complexity -- O(n^2)
+
+	Space complexity -- O(n)
+
+*/
+
+
+
